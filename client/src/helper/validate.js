b03a7db639e880997f9fd739e48f7df644a61f68
MSI <hirun08072002@gmail.com>
2024-11-12 08:12:33 +0530

    changes

--- client/src/helper/validate.js
+++ client/src/helper/validate.js
@@ -77,5 +77,9 @@ function usernameVerify(error = {}, values){
 function emailVerify(error = {},values){
     if (!values.email) {
         error.email = toast.error("Email required...!");
+    }else if (values.email.includes("")) {
+        error.email = toast.error("wrong email...!")
+    }else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+        error.email = toast.error("Invalid email....!")
     }
 }
