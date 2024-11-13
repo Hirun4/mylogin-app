d343fafcc2caeaa2ed16119c5b6ab8d6e49b6d16
MSI <hirun08072002@gmail.com>
2024-11-13 19:42:46 +0530

    import

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
 import connect from './database/conn.js';
+import router from './router/route.js';
 
 
 const app = express();
