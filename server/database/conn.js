3fc82158522f45edf82f9c50ef05a6fe53707050
MSI <hirun08072002@gmail.com>
2024-11-13 17:58:15 +0530

    connection databse

--- server/database/conn.js
+++ server/database/conn.js
@@ -2,7 +2,8 @@ import mongoose from "mongoose";
 import { MongoMemoryServer } from "mongodb-memory-server";
 
 async function connect(params) {
-    
+    const mongod = await MongoMemoryServer.create();
+    const getUri = mongod.getUri();
 }
 
 export default connect;
