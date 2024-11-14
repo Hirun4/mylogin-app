45c042f6be575da64d946218e1f10f4fd5552356
MSI <hirun08072002@gmail.com>
2024-11-14 07:53:11 +0530

    routing

--- server/router/route.js
+++ server/router/route.js
@@ -22,7 +22,7 @@ router.route('/createResetSession').get(controller.createResetSession);
 
 /*PUT methods*/
 router.route('/updateuser').put(controller.updateUser);
-router.route('resetPassword').put();
+router.route('resetPassword').put(controller.resetPassword);
 
 
 export default router;
