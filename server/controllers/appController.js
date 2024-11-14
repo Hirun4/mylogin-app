48b335de2dc7cd1aae2b8fa65e13cf5b0bfb8771
MSI <hirun08072002@gmail.com>
2024-11-14 07:30:05 +0530

    new cn

--- server/controllers/appController.js
+++ server/controllers/appController.js
@@ -9,3 +9,7 @@ export async function register(req,res) {
 export async function login(req,res) {
     res.json('login route');
 }
+
+export async function getUser(req,res) {
+    res.json('getUser route');
+}
