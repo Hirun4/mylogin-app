6d511bf863615b0dad9c4b4e165cd72aa94ddf1a
MSI <hirun08072002@gmail.com>
2024-11-14 07:45:37 +0530

    routing

--- server/router/route.js
+++ server/router/route.js
@@ -7,8 +7,8 @@ import * as controller from '../controllers/appController.js';
 
 
 /*Post methods*/
-router.route('/register').post((req,res) => res.json('register route'));
-router.route('/registerMail').post();
+router.route('/register').post(controller.register);
+// router.route('/registerMail').post();
 router.route('/authenticate').post();
 router.route('/login').post();
 
