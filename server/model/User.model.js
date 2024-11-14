78c08056838c253345c0acf17919acc901f640ac
MSI <hirun08072002@gmail.com>
2024-11-14 08:33:43 +0530

    username

--- server/model/User.model.js
+++ server/model/User.model.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 export const UserSchema = new mongoose.Schema({
     username: {
         type: String,
-        required: [true, "please provide unique username"]
+        required: [true, "please provide unique username"],
+        unique: [true, "username exist"]
     }
 })
