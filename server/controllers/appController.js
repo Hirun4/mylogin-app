9c47670507ccd45816dc7bf6a1071b2ca62e7f25
Hirun Mihisara Kariyawasam <hirun08072002@gmail.com>
2024-11-22 17:04:24 +0530

    add new getuser function

--- server/controllers/appController.js
+++ server/controllers/appController.js
@@ -165,7 +165,7 @@ export async function login(req, res) {
   }
 }
 
-// export async function getUser(req, res) {
+//export async function getUser(req, res) {
 //   const { username } = req.params;
 
 //   try {
