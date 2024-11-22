f7e49e64fedfd8344d316ffafe87384950f5f7a4
jayathu sanka <shjayathusanka@gmail.com>
2024-11-22 21:25:16 +0530

    reset finish and mail continue

--- server/router/route.js
+++ server/router/route.js
@@ -3,12 +3,14 @@ const router= Router();
 
 /*import all controllers*/
 import * as controller from '../controllers/appController.js';
+import { registerMail } from "../controllers/mailer.js";
 import Auth, { localVariables } from "../middleware/auth.js";
+
 
 
 /*Post methods*/
 router.route('/register').post(controller.register);
-// router.route('/registerMail').post();
+router.route('/registerMail').post(registerMail);
 router.route('/authenticate').post((req,res) => res.end());
 router.route('/login').post(controller.verifyUser,controller.login);
 
