c58be439a04dbb4ce78f43b91fa93951c149cba8
jayathu sanka <shjayathusanka@gmail.com>
2024-11-23 19:15:12 +0530

    Let's go and go to password page

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,2 +1,11 @@
-import create from 'zustand';
+//import create from 'zustand';
+import { create } from 'zustand';
+import Username from '../components/Username';
 
+export const useAuthStore = create((set) =>({
+    auth : {
+        Username : '',
+        active : false
+    },
+    setUsername : (name) => set((state) => ({ auth : {...state.auth, username : name }}))
+}))
