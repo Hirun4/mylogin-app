0ba83f276b65a691b09211c40349542381f344eb
Hirun Mihisara Kariyawasam <hirun08072002@gmail.com>
2024-11-24 12:49:57 +0530

    usefetch

--- client/src/hooks/fetch.hook.js
+++ client/src/hooks/fetch.hook.js
@@ -16,6 +16,12 @@ export default function useFetch(query){
                 setData(prev =>({...prev, isLoading: true}) )
 
                 const{data,status} = await axios.get(`/api/${query}`);
+
+                if(status === 201){
+                    setData(prev =>({...prev, isLoading: false}) )
+                    setData(prev =>({...prev, apiData: data , status: status}) )
+                }
+                setData(prev =>({...prev, isLoading: false}) )
             } catch (error) {
                 setData(prev =>({...prev, isLoading: false, severError: error}) )
             }
