e5e6980ad0083e333def947de6358f5b4f2251ae
Hirun Mihisara Kariyawasam <hirun08072002@gmail.com>
2024-11-24 13:24:05 +0530

    changes

--- client/src/components/Password.js
+++ client/src/components/Password.js
@@ -39,7 +39,7 @@ export default function Password() {
             <div className={styles.glass}>
               
               <div className="title flex flex-col items-center">
-                <h4 className='text-5xl font-bold'>Hello Again!</h4>
+                <h4 className='text-5xl font-bold'>Hello {apiData?.firstName}!</h4>
                 <span className='py-4 text-xl w-2/3 text-center text-gray-500'>
                 Explore More by connecting with us.
                 </span>
