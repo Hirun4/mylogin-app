b15ceeb7728de0cdb0b2539eacde3509daba87de
Hirun Mihisara Kariyawasam <hirun08072002@gmail.com>
2024-11-24 12:01:10 +0530

    useFetch

--- client/src/hooks/fetch.hook.js
+++ client/src/hooks/fetch.hook.js
@@ -1,4 +1,22 @@
 import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 
 axios.defaults.baseURL = process.env.REACT_APP_SERVER_DOMAIN;
+
+
+export default function useFetch(query){
+    const[getData,setData] = useState({isLoading : false,apiData : undefined,status: null,severError: null})
+
+    useEffect(() => {
+        if(!query) return;
+
+        const fetchData = async () => {
+            try {
+                
+            } catch (error) {
+                setData(prev =>({...prev, isLoading: false}) )
+            }
+        }
+    })
+}
