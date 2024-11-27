214ce12a7003319ae6985c9349e12178111f5737
Hirun Mihisara Kariyawasam <hirun08072002@gmail.com>
2024-11-27 16:09:48 +0530

    add mongodb uri

--- server/config.js
+++ server/config.js
@@ -1,5 +1,6 @@
 export default{
     JWT_SECRET : "TXNQzh7dckQ/Kb7WXhw2IaYI/cHd7y7VRezaLHeW0Qs=",
     EMAIL: "jamie.schultz16@ethereal.email",//Testing email and password
-    PASSWORD : "AbCQSyq17pm1mVfBDJ"
+    PASSWORD : "AbCQSyq17pm1mVfBDJ",
+    ATLAS_URI: "mongodb+srv://hirun08072002:hirun08072002@login.wmz8j.mongodb.net/"
 }
