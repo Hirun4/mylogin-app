b1f39adf628bcfafd11b7537772e7c2d1909c26f
Hirun Mihisara Kariyawasam <hirun08072002@gmail.com>
2024-11-27 21:00:34 +0530

    config setup

--- server/config.js
+++ server/config.js
@@ -2,5 +2,5 @@ export default{
     JWT_SECRET : "TXNQzh7dckQ/Kb7WXhw2IaYI/cHd7y7VRezaLHeW0Qs=",
     EMAIL: "jamie.schultz16@ethereal.email",//Testing email and password
     PASSWORD : "AbCQSyq17pm1mVfBDJ",
-    ATLAS_URI: "mongodb+srv://hirun08072002:hirun08072002@login.wmz8j.mongodb.net/"
+    ATLAS_URI: "mongodb+srv://hirun08072002:hirun08072002@cluster0.qpqk8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 }
